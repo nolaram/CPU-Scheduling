392b6ffd0d835b800df5fa9818401b0d097955a3
nolaram <marloncopino10@gmail.com>
2026-06-01 21:30:56 +0800

    add_process

--- cpu_scheduling.py
+++ cpu_scheduling.py
@@ -1,7 +1,12 @@
 # Hello Ninjas
 
 # First Come First Serve (FCFS) Scheduling Algorithm
-processes = [{"process_id": "P1", "arrival": 0, "burst": 5}, {"process_id": "P2", "arrival": 2, "burst": 3},]
+processes = [{"process_id": "P1", "arrival": 0, "burst": 5}, 
+             {"process_id": "P2", "arrival": 2, "burst": 3},
+             {"process_id": "P3", "arrival": 4, "burst": 1},
+             {"process_id": "P4", "arrival": 6, "burst": 2},
+             {"process_id": "P5", "arrival": 8, "burst": 4}
+             ]
 
 current_time = 0
 
@@ -14,6 +19,8 @@ for process in processes:
    process["current_time"]  = current_time
    process["turnaround_time"] = process["current_time"] - process["arrival"]
    process["waiting_time"]  = process["turnaround_time"] - process["burst"]
+
+print(f"{'Process ID'} {'Current Time'} {'Turnaround Time'} {'Waiting Time'}")
 
 for process in processes:
     print(f"{process['process_id']}  CT={process['current_time']}  TAT={process['turnaround_time']}  WT={process['waiting_time']}") 
