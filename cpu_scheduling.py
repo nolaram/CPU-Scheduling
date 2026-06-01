00afe442715dc1bec27e7996f570368cef1abcaa
nolaram <marloncopino10@gmail.com>
2026-06-01 19:48:17 +0800

    fcfs_basics

--- cpu_scheduling.py
+++ cpu_scheduling.py
@@ -1,1 +1,15 @@
 # Hello Ninjas
+
+# First Come First Serve (FCFS) Scheduling Algorithm
+
+arrival = 0
+burst = 5
+current_time = 0
+
+current_time += burst
+completion = current_time
+
+turnaround_time = completion - arrival
+waiting_time = turnaround_time - burst
+
+print(f"CT={completion}, TAT={turnaround_time}, WT={waiting_time}")
